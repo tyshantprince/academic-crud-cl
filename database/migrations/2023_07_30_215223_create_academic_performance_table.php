966375042d8a045ebd6ff83a449229df2f3b3740
tyshantprince <tyshantprince@gmail.com>
2023-07-30 18:06:33 -0500

    init migration

---
+++ database/migrations/2023_07_30_215223_create_academic_performance_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('academic_performances', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('student_group');
+            $table->string('subject');
+            $table->date('date');
+            $table->integer('assessment_score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('academic_performances');
+    }
+};
+        
+
